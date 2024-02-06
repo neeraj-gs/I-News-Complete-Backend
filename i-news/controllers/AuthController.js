d1b28f0870db991116f2f32e69bf76cec35b76c5
neeraj-gs <gsneeraj05@gmail.com>
2024-02-06 21:58:13 +0530

    Added Prisma Migrations into daatbse and installed Prisma CLient

---
+++ i-news/controllers/AuthController.js
@@ -0,0 +1,5 @@
+class AuthController {
+    static async register(req,res)=>{
+        const body = req.body;
+    } //if we add static we can access usign . , else we need to create a new instnce
+}
